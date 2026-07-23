1aacb4456d81c91e33428afbfbbf566aac3c1748
luxiaoshi838-jpg <luxiaoshi838@gmail.com>
2026-07-23 16:13:00 +0800

    Make palette toggle testable without Android color stubs

--- app/src/main/java/com/simplereader/app/ui/ReaderAppearance.kt
+++ app/src/main/java/com/simplereader/app/ui/ReaderAppearance.kt
@@ -65,9 +65,9 @@ object ReaderAppearance {
     }
 
     fun isDark(color: Int): Boolean {
-        val r = Color.red(color)
-        val g = Color.green(color)
-        val b = Color.blue(color)
+        val r = (color ushr 16) and 0xFF
+        val g = (color ushr 8) and 0xFF
+        val b = color and 0xFF
         return (0.299 * r + 0.587 * g + 0.114 * b) < 96
     }
 }
