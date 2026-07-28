46307e6fc1d9bcba32755fc299d8b8201d56d13e
luxiaoshi838-jpg <luxiaoshi838@gmail.com>
2026-07-28 18:15:47 +0800

    Upgrade build toolchain for Readium

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import com.android.build.api.dsl.ApplicationExtension
 
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.android.application") version "8.6.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
 subprojects {
