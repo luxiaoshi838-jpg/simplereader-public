56493ce8e16996b3eaf06787f1719f43766ad161
luxiaoshi838-jpg <luxiaoshi838@gmail.com>
2026-08-27 17:14:03 +0800

    v725: keep application startup chain minimal

--- app/src/main/java/com/simplereader/app/App.kt
+++ app/src/main/java/com/simplereader/app/App.kt
@@ -2,7 +2,6 @@ package com.simplereader.app
 
 import android.app.Application
 import com.simplereader.app.crash.CrashLogStore
-import com.simplereader.app.operation.OperationLogUiInstaller
 
 class App : Application() {
     companion object {
@@ -13,11 +12,6 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
-
-        // v724: do not insert operation-log migration into the application startup chain.
-        // Legacy v721/v722 operation storage is purged lazily by OperationLogStore before
-        // the new bounded operation history is first read or written.
         CrashLogStore.install(this)
-        OperationLogUiInstaller.install(this)
     }
 }
